9a72aab8051d332a43fed71a5bfa2a9a150f32f8
android <asdf>
2023-04-18 12:19:24 -0400

    update

--- app/src/main/java/com/example/afinal/Floor.java
+++ app/src/main/java/com/example/afinal/Floor.java
@@ -2,13 +2,34 @@ package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Floor extends AppCompatActivity {
+
+    ImageButton buttonRed;
+    ImageButton buttonCar;
+    ImageButton buttonBW;
+    ImageButton buttonWood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor);
+
+        buttonRed=findViewById(R.id.buttonRed);
+        buttonCar=findViewById(R.id.buttonCar);
+        buttonBW=findViewById(R.id.buttonBW);
+        buttonWood=findViewById(R.id.buttonWood);
+
+        buttonRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Tables.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/afinal/Pizza.java
+++ app/src/main/java/com/example/afinal/Pizza.java
@@ -3,12 +3,18 @@ package com.example.afinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Pizza extends AppCompatActivity {
+
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizza);
+
+        button4=findViewById(R.id.button4);
+
     }
 }

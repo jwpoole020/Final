9091da6ba321c0761c4bc17db1faac3ee32c60d4
android <asdf>
2023-04-27 11:23:34 -0400

    updates

--- app/src/main/java/com/example/afinal/Tables.java
+++ app/src/main/java/com/example/afinal/Tables.java
@@ -6,22 +6,23 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Tables extends AppCompatActivity {
 
-    ImageButton buttonCir;
-    ImageButton buttonBooth;
+    Button buttonBooth;
+    Button buttonTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tables);
 
-        buttonCir=findViewById(R.id.buttonCir);
+        buttonTable=findViewById(R.id.buttonTable);
         buttonBooth=findViewById(R.id.buttonBooth);
 
-        buttonCir.setOnClickListener(new View.OnClickListener() {
+        buttonTable.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), Pizza.class);

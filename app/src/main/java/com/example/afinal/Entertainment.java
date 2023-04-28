9d4d6f852c015c6750463cfd05c3df65ed794039
Jacobo <jwpoole020@gmail.com>
2023-04-27 20:16:37 -0400

    Updates :P

--- app/src/main/java/com/example/afinal/Entertainment.java
+++ app/src/main/java/com/example/afinal/Entertainment.java
@@ -3,10 +3,32 @@ package com.example.afinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Entertainment extends AppCompatActivity {
 
-    ImageView
+    ImageView imageMachine;
+    ImageView imageAni;
+    ImageView imageBP;
+    ImageView imageArcade;
+    RadioGroup groupMachine;
+    RadioGroup groupAni;
+    RadioGroup groupBP;
+    RadioGroup groupArcade;
+    RadioButton ;
+    RadioButton;
+    RadioButton;
+    RadioButton;
+    RadioButton;
+    RadioButton;
+    RadioButton;
+    RadioButton;
+    RadioButton;
+    RadioButton;
+    RadioButton;
+    RadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
